a30b2ad13e7c0e5296026d7f015ef11b2b50c428
Alfredo08 <alfredosv08@gmail.com>
2022-02-09 08:25:25 -0600

    Agregando fibonacci recursivo y palindromo recursi

--- fibonacciRecursivo.py
+++ fibonacciRecursivo.py
@@ -2,3 +2,13 @@
 # número entero positivo. La función se encarga de imprimir el número recibido de la serie de fibonacci.
 # Ejemplo 10 => 34
 # Serie = 0 1 1 2 3 5 8 13 21 34 55 89
+
+def fibonacciRecursivo( num ):
+    if num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        return fibonacciRecursivo( num - 1 ) + fibonacciRecursivo( num - 2 )
+
+print( fibonacciRecursivo( 5 ) )
--- palindromoRecursivo.py
+++ palindromoRecursivo.py
@@ -1,3 +1,14 @@
 # Escribe una función llamada palindromoRecursivo. Esta función recibe como parámetro
 # un string. La función se encarga de retornar True si el string es un palindromo
 # False de lo contrario.
+
+def palindromoRecursivo( texto ):
+    if len( texto ) <= 1:
+        return True
+    else:
+        if texto[0] != texto[ len(texto) - 1 ]:
+            return False
+        else:
+            return palindromoRecursivo( texto[1:-1] )
+
+print( palindromoRecursivo( "radar" ) )
